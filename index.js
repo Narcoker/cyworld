59f58660b778d22afdbd6fc2567ffadaaac1e40b
Narcoker <jykim9868@gmail.com>
2023-01-27 18:10:03 +0900

    🚑 Fix: 네비게이션 포커싱 에러 수정

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ const menuGame = () => {
     document.getElementById("contentFrame").setAttribute("src", "game.html");
     document.getElementById("menuGame").style = "color: black; background-color: white;";
     document.getElementById("menuHome").style = "color: white; background-color: #298eb5;";
-    document.getElementById("menuGame").style = "color: white; background-color: #298eb5;";
+    document.getElementById("menuJukebox").style = "color: white; background-color: #298eb5;";
 
 }
